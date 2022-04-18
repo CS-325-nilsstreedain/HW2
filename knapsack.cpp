a9ef0bea66880a8a5f181c9e1c1a73cbb672e32d
Nils Streedain <tannins_berets_0@icloud.co>
2022-04-17 19:35:07 -0700

    Completed recursive and dynamic programming functions

--- knapsack.cpp
+++ knapsack.cpp
@@ -8,16 +8,55 @@
 #include <iostream>
 
 int recursive(int W, int n, int val[], int wt[]) {
-	return 0;
+	// Recursive base case
+	if (W == 0 || n ==0)
+		return 0;
+	
+	// Item k is too big to fit with cap W
+	if (wt[n - 1] > W)
+		return recursive(W, n - 1, val, wt);
+	// Else, return larger subset containing or not containing item k
+	else {
+		int nCont = val[n - 1] + recursive(W - wt[n - 1], n - 1, val, wt);
+		int nNot = recursive(W, n - 1, val, wt);
+		return (nCont > nNot) ? nCont : nNot;
+	}
 }
 
-int dynamic(int W, int n, int val[], int wt[]) {
+int dynamic(int W, int n, int *val, int *wt) {
+	int K[n + 1][W + 1];
 	
-	return 0;
+	int i, j;
+	for (i = 0; i <= n; i++) {
+		for (j = 0; j <= W; j++) {
+			if (i == 0 || j == 0)
+				K[i][j] = 0;
+			else if (wt[i - 1] > j)
+				K[i][j] = K[i - 1][j];
+			else {
+				int nCont = val[i - 1] + K[i - 1][j - wt[i - 1]];
+				int nNot = K[i - 1][j];
+				K[i][j] = (nCont > nNot) ? nCont : nNot;
+			}
+		}
+	}
+	
+	return K[n][W];
+}
+
+template<typename Function>
+double timer(Function func, int *max, int W, int n, int *val, int *wt) {
+	clock_t start, end;
+	start = clock();
+	*max = func(W, n, val, wt);
+	end = clock();
+	return (double)(end - start)/CLOCKS_PER_SEC;
 }
 
 int main(int argc, const char * argv[]) {
 	srand((int)time(0));
+	
+	//Initalize general vars
 	int W = 100;
 	
 	for (int n = 10; n <= 50; n += 5) {
@@ -31,20 +70,11 @@ int main(int argc, const char * argv[]) {
 			wt[i] = rand() % 50;
 		}
 		
-		// Initilize clock vars
-		clock_t rStart, rEnd, dpStart, dpEnd;
-		
-		// Recursive timing
-		rStart = clock();
-		recursive(W, n, val, wt);
-		rEnd = clock();
-		
-		// Dynamic timing
-		dpStart = clock();
-		dynamic(W, n, val, wt);
-		dpEnd = clock();
+		int rMax, dpMax;
+		double rTime = timer(recursive, &rMax, W, n, val, wt);
+		double dpTime = timer(dynamic, &dpMax, W, n, val, wt);
 		
 		// Output
-		printf("N=%i\tW=%i\tRec time=%f\tDP time=%f\tmax Rec=%i\tmaxDP=%i\n", n, W, (double)(rEnd - rStart)/CLOCKS_PER_SEC, (double)(dpEnd - dpStart)/CLOCKS_PER_SEC, recursive(W, n, val, wt), dynamic(W, n, val, wt));
+		printf("N=%i\tW=%i\tRec time=%f\tDP time=%f\tmax Rec=%i\tmaxDP=%i\n", n, W, rTime, dpTime, rMax, dpMax);
 	}
 }
